e4b92f259b0eaa8cc2f2367aa60e67bf5086d107
jungmin <43zhuzhu@gmail.com>
2023-02-17 10:54:30 +0900

    Props 추가공부

---
+++ src/components/Child1.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav>
+        <ul class="menu">
+            <li class="item" v-for="(item) in propsItem" v-bind:key="item.id">
+                <a v-bind:href="'/' + item.title">{{ item.title }}</a>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Child1',
+    props: {
+        propsItem : { type: Object, default: () => {
+            return {
+                id: '',
+                title: '',
+            }
+        } }
+    }
+}
+</script>
+<style>
+.menu {display:flex; justify-content:center; padding:10px; border-radius:5px; background:steelblue; list-style:none;}
+.item {padding:10px;}
+.item a {color:#fff; text-decoration:none;}
+</style>
---
+++ src/components/Child2.vue
@@ -0,0 +1,22 @@
+<template>
+    <li class="product-item">
+        <img v-bind:src="propProduct.img" v-bind:alt="propProduct.title"/>
+        <div class="product-content">
+            <h3 class="title">{{ propProduct.title }}</h3>
+        </div>
+    </li>
+</template>
+
+<script>
+export default {
+    name: 'Child2',
+    props: {
+        propProduct: { type: Object, default: () => {} }
+    }
+}
+</script>
+
+<style>
+.product-item {display:flex; flex-direction:row; justify-content:center; flex-wrap:wrap; align-items:center; list-style:none; margin:10px;}
+.product-content {margin-left:10px;}
+</style>
